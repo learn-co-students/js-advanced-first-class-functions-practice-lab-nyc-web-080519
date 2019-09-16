45df0e5b473c05b0ddf3cfc044b8f1895d2d16c9
tomjmcnamee <tom@tomjmcnamee.com>
2019-09-16 17:27:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+
+
+const logDriverNames = function (arr) {
+  arr.forEach( function(drvr) { console.log(drvr.name)});
+}
+
+const logDriversByHometown = function (arr, location) {
+  arr.forEach(function (drvr) { if (drvr.hometown === location) { 
+    console.log(drvr.name) };
+  }  )}
+
+
+
+const driversByRevenue = function (arr) {
+    return arr.slice().sort( function (a, b) { return a.revenue - b.revenue;}  );
+    return output;
+  }
+
+
+ 
+const driversByName = function (arr) {
+    return arr.slice().sort( function (a,b) {return a.name.localeCompare(b.name)}   );
+  }
+
+// totalRevenue() — Receives an array of driver objects and returns the sum of the revenue earned by each driver.
+const totalRevenue = function (arr) {
+  let totalPrice = 0;
+  arr.forEach(function (driver) {
+    totalPrice += driver.revenue;
+  })
+  return totalPrice;
+}
+
+const averageRevenue = function (arr) {
+  let num_of_drivers = arr.length;
+  return totalRevenue(arr)/num_of_drivers;
+}
+
+ 
